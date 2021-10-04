db08792b0164757e3afcfe7245a7162787437ca5
michael <ittybemain@gmail.com>
2021-10-04 20:01:32 +0200

    create quotes class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import Quotes from "./Quotes";
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="main-bg bg-m-light-main">
-      <div className="app">
-        
+
+export class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      quotes: [
+        {
+          text: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+          author: "James Cameron"
+        },
+        {
+          text: "If life were predictable it would cease to be life, and be without flavor.",
+          author: "Eleanor Roosevelt"
+        },
+        {
+          text: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+          author: "Steve Jobs"
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="main-bg bg-m-light-main">
+        <div className="app">
+          <Quotes quotes={this.state.quotes} />
+        </div>
       </div>
-    </div>
-    
-  );
+    );
+  }
+  
 }
 
 export default App;
--- src/Quotes.js
+++ src/Quotes.js
@@ -1,8 +1,64 @@
 import React from "react";
+import refresh from "./assets/desktop/icon-refresh.svg";
+import "./Quotes.css";
 
 export class Quotes extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
 
+        let firstQuote = null
+        if (this.props.quotes.length > 0){
+            firstQuote = this.props.quotes[0];
+        }
+        else {
+            console.log("no quotes in props")
+        }
+        
+        this.state = {
+            currentQuote: {
+                text: firstQuote.text,
+                author: firstQuote.author,
+                index: 0
+            }
+        }
+    }
+
+    updateQuote() {
+        if (this.props.quotes.length === this.state.currentQuote.index + 1) {
+            const firstQuote = this.props.quotes[0];
+            const currentQuote = {
+                text: firstQuote.text,
+                author: firstQuote.author,
+                index: 0
+            }
+
+            this.setState({currentQuote: currentQuote})
+        }
+        else {
+            const index = this.state.currentQuote.index + 1;
+            const nextQuote = this.props.quotes[index];
+            const currentQuote = {
+                text: nextQuote.text,
+                author: nextQuote.author,
+                index: index
+            }
+
+            this.setState({currentQuote: currentQuote})
+        }
+    }
+
+    render() {
+        return (
+            <div className="flex flex-row ">
+                <div>
+                    <div className="">"{this.state.currentQuote.text}"</div>
+                    <div className="">{this.state.currentQuote.author}</div>
+                </div>
+                <button className="refresh-icon flex-shrink-0" onClick={() => this.updateQuote()}>
+                    <img src={refresh} alt="refresh"/>
+                </button>
+            </div>
+        ) 
     }
 }
 
