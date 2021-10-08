f77425edf8f6e0a1f22e1b912c7c7d70caab4f34
michael <ittybemain@gmail.com>
2021-10-08 19:03:43 +0200

    clock timer synchronization

--- src/Clock.js
+++ src/Clock.js
@@ -16,27 +16,28 @@ class Time {
             return null;
         }
     }
-
+    
     async getTimeByIp() {
         try {
             const ip = await this.getIp();
-            const res = await axios.get(`http://worldtimeapi.org/api/ip/${ip}`);
+            if (ip){
+                const res = await axios.get(`http://worldtimeapi.org/api/ip/${ip}`);
             
-            // convert datetime into string with only hours and minutes
-            const d = Date.parse(res.data.datetime);
-            console.log(`datetime: ${d}`)
-            
-            const formatedTime = moment(d).format("HH:mm")
-            console.log(`formatedTime: ${formatedTime}`)
-            
-            console.log(res.data)
-            return {
-                timezone: res.data.timezone,
-                abbreviation: res.data.abbreviation,
-                dayOfWeek: res.data.day_of_week,
-                dayOfYear: res.data.day_of_year,
-                weekNumber: res.data.week_number,
-                time: formatedTime
+                // convert datetime into string with only hours and minutes
+                const d = Date.parse(res.data.datetime);
+                
+                const formatedTime = moment(d).format("HH:mm")
+
+                console.log("time by ip", res)
+                return {
+                    timezone: res.data.timezone,
+                    abbreviation: res.data.abbreviation,
+                    dayOfWeek: res.data.day_of_week,
+                    dayOfYear: res.data.day_of_year,
+                    weekNumber: res.data.week_number,
+                    datetime: res.data.datetime,
+                    formatedTime: formatedTime
+                }
             }
         } catch (error) {
             return {
@@ -44,8 +45,8 @@ class Time {
                 abbreviation: "",
                 dayOfWeek: "",
                 dayOfYear: "",
-                weekNumber: "",
-                time: "error"
+                datetime: "",
+                formatedTime: "error"
             }
         }
     }
@@ -59,16 +60,46 @@ export class Clock extends React.Component {
             timeInfo: {} 
         }
         this.time = new Time();
+        this.intervalId = null;
+    }
+
+    componentDidMount() {
         this.initialize();
+        this.updateClock = this.updateClock.bind(this);
     }
 
     async initialize() {
+        const timeInfo =  await this.time.getTimeByIp();
+        timeInfo.timezone = timeInfo.timezone.replace(/[/]/g, ", ");
+        console.log(`init ${JSON.stringify(timeInfo)}`);
+        this.setState({timeInfo: timeInfo});
+        
+        const timestamp = (Date.parse(timeInfo.datetime));
+        const d = new Date(timestamp)
+        const seconds = d.getSeconds()
+        const deltaSeconds = 60 - seconds; 
+        console.log(`delta seconds=${deltaSeconds}, seconds = ${seconds}`)
+        setTimeout(this.updateClock, deltaSeconds*1000);
+        this.intervalId = setInterval(this.updateClock, 60000);
+    } 
+    
+    async asyncUpdateClock() {
+        console.log("updating")
         const timeInfo =  await this.time.getTimeByIp()
         timeInfo.timezone = timeInfo.timezone.replace(/[/]/g, ", ")
-        console.log(`init, ${JSON.stringify(timeInfo)}`)
-        this.setState({timeInfo: timeInfo});
-    } 
+        if (timeInfo.time !== this.state.timeInfo.formatedTime){
+            console.log(`updated time=${timeInfo.formatedTime}`);
+            this.setState({timeInfo: timeInfo});
+        }
+        else {
+            console.log(`not updated time=${timeInfo.formatedTime}`)
+        }
+    }
 
+    updateClock() {
+        this.asyncUpdateClock()
+    }
+    
     render() {
         return (
             <div className="flex-auto flex flex-col justify-end">
@@ -79,15 +110,16 @@ export class Clock extends React.Component {
                 </div>
                 <div className="
                     text-m-time-f-s 
-                    text-bold 
+                    font-bold 
                 ">
-                    {this.state.timeInfo.time}
-                    <div className="text-m-abbr-f-s">{this.state.timeInfo.abbreviation}</div>
+                    {this.state.timeInfo.formatedTime}
+                    <span className="text-m-abbr-f-s font-light">{this.state.timeInfo.abbreviation}</span>
                 </div>
                 <div className="
                     text-m-greeting-f-s
+                    font-bold
                 ">
-                    in {this.state.timeInfo.timezone}
+                    IN {this.state.timeInfo.timezone}
                 </div>
             </div>
         )
