27eeb2083fc35bca7071ab0fe4912c34d02a8583
michael <ittybemain@gmail.com>
2021-10-09 18:57:18 +0200

    change basic principles of updating time

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import Quotes from "./Quotes";
 import './App.css';
 import React from "react";
 import Clock from "./Clock";
+import axios from "axios";
+
+
+async function getGeo() {
+  const res = await axios.get('https://geolocation-db.com/json/')
+  console.log("geo: ", res.data);
+  if (res.data.IPv4) {
+    return {
+      ip: res.data.IPv4,
+      countryCode: res.data.country_code,
+      city: res.data.city
+    };
+  }
+  else {
+    return null;
+  }
+}
+
+async function getTimeByIp(ip) {
+  if (ip) {
+    const res = await axios.get(`http://worldtimeapi.org/api/ip/${ip}`);
+
+    // convert datetime into string with only hours and minutes
+    console.log("time by ip", res)
+
+    return {
+      timezone: res.data.timezone,
+      abbreviation: res.data.abbreviation,
+      dayOfWeek: res.data.day_of_week,
+      dayOfYear: res.data.day_of_year,
+      weekNumber: res.data.week_number,
+      unixtime: res.data.unixtime,
+    }
+  }
+}
 
 export class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-
     this.state = {
       quotes: [
         {
@@ -21,22 +55,65 @@ export class App extends React.Component {
           text: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
           author: "Steve Jobs"
         },
-      ]
+      ],
+      isMorning: true,
+      timeInfo: {},
+      unixtime: null
+    }
+    
+  }
+  componentDidMount() {
+    this.initialize().then(() => {
+      setInterval(() => { 
+        this.setState({ unixtime: this.state.unixtime + 1 })
+        console.log("set interval")
+      }, 1000)}, () => { }
+    )
+  }
+
+  async initialize() {
+    const timeInfo = await this.getTimeInfo();
+    this.setState({ timeInfo: timeInfo, unixtime: timeInfo.unixtime })
+    console.log(`unixtime init: `, timeInfo.unixtime)
+  }
+
+
+  async getTimeInfo() {
+    const geo = await getGeo();
+    const time = await getTimeByIp(geo.ip);
+
+    return {
+      ip: geo.ip,
+      countryCode: geo.countryCode,
+      city: geo.city,
+      timezone: time.timezone,
+      abbreviation: time.abbreviation,
+      dayOfWeek: time.dayOfWeek,
+      dayOfYear: time.dayOfYear,
+      weekNumber: time.weekNumber,
+      unixtime: time.unixtime,
     }
   }
+
+
   render() {
     return (
       <div className="main-bg bg-m-light-main">
         <div className="app ">
           <div className="flex flex-col m-7 h-full">
             <Quotes quotes={this.state.quotes} />
-            <Clock/>
+            <Clock
+              abbreviation={this.state.timeInfo.abbreviation}
+              unixtime={this.state.unixtime}
+              city={this.state.timeInfo.city}
+              countryCode={this.state.timeInfo.countryCode}
+              isMorning={this.state.isMorning} />
           </div>
         </div>
       </div>
     );
   }
-  
+
 }
 
 export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,110 +1,24 @@
 import React from "react";
-import axios from "axios";
 import moment from "moment";
-import "./Clock.css" 
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-class Time {
-    async getIp() {
-        const res = await axios.get('https://geolocation-db.com/json/')
-        console.log(res.data);
-        if (res.data.IPv4){
-            return res.data.IPv4;
-        }
-        else {
-            return null;
-        }
-    }
-    
-    async getTimeByIp() {
-        try {
-            const ip = await this.getIp();
-            if (ip){
-                const res = await axios.get(`http://worldtimeapi.org/api/ip/${ip}`);
-            
-                // convert datetime into string with only hours and minutes
-                const d = Date.parse(res.data.datetime);
-                
-                const formatedTime = moment(d).format("HH:mm")
-
-                console.log("time by ip", res)
-                return {
-                    timezone: res.data.timezone,
-                    abbreviation: res.data.abbreviation,
-                    dayOfWeek: res.data.day_of_week,
-                    dayOfYear: res.data.day_of_year,
-                    weekNumber: res.data.week_number,
-                    datetime: res.data.datetime,
-                    formatedTime: formatedTime
-                }
-            }
-        } catch (error) {
-            return {
-                timezone: "",
-                abbreviation: "",
-                dayOfWeek: "",
-                dayOfYear: "",
-                datetime: "",
-                formatedTime: "error"
-            }
-        }
-    }
-}
-
+import "./Clock.css"
+// TODO error handling in Time class
 
 export class Clock extends React.Component {
-    constructor(props) {
-        super(props)        
-        this.state = {
-            timeInfo: {},
-            isMorning: true
-        }
-        this.time = new Time();
-        this.intervalId = null;
+    getFormatedTime() {
+        const milliseconds = this.props.unixtime * 1000;
+        const d = new Date(milliseconds);
+
+        const formatedTime = moment(d).format("HH:mm");
+
+        return formatedTime;
     }
 
-    componentDidMount() {
-        this.initialize();
-        this.updateClock = this.updateClock.bind(this);
+    getLocation() {
+        return `IN ${this.props.city}, ${this.props.countryCode}`.toUpperCase();
     }
 
-    async initialize() {
-        const timeInfo =  await this.time.getTimeByIp();
-        timeInfo.timezone = timeInfo.timezone.replace(/[/]/g, ", ").toUpperCase();
-        console.log(`init ${JSON.stringify(timeInfo)}`);
-        this.setState({timeInfo: timeInfo});
-        
-        const timestamp = (Date.parse(timeInfo.datetime));
-        const d = new Date(timestamp);
-        const seconds = d.getSeconds();
-        const deltaSeconds = 60 - seconds; 
-        console.log(`delta seconds=${deltaSeconds}, seconds = ${seconds}`);
-        sleep(deltaSeconds * 1000);
-        this.intervalId = setInterval(this.updateClock, 60000);
-    } 
-    
-    async asyncUpdateClock() {
-        console.log("updating")
-        const timeInfo =  await this.time.getTimeByIp()
-        timeInfo.timezone = timeInfo.timezone.replace(/[/]/g, ", ").toUpperCase();
-        if (timeInfo.time !== this.state.timeInfo.formatedTime){
-            console.log(`updated time=${timeInfo.formatedTime}`);
-            this.setState({timeInfo: timeInfo});
-        }
-        else {
-            console.log(`not updated time=${timeInfo.formatedTime}`)
-        }
-    }
-
-    updateClock() {
-        this.asyncUpdateClock()
-    }
-    
     getGreeting() {
-        return this.state.isMorning ? "GOOD MORNING" : "GOOD EVENING";
+        return this.props.isMorning ? "GOOD MORNING" : "GOOD EVENING";
     }
 
     render() {
@@ -122,15 +36,15 @@ export class Clock extends React.Component {
                     text-m-time-f-s 
                     font-bold 
                 ">
-                    {this.state.timeInfo.formatedTime}
-                    <span className="text-m-abbr-f-s font-light">{this.state.timeInfo.abbreviation}</span>
+                    {this.getFormatedTime()}
+                    <span className="text-m-abbr-f-s font-light">{this.props.abbreviation}</span>
                 </div>
                 <div className="
                     text-m-greeting-f-s
                     font-bold
                     tracking-widest
                 ">
-                    IN {this.state.timeInfo.timezone}
+                    {this.getLocation()}
                 </div>
             </div>
         )
